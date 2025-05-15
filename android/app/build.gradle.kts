d39a291e5d67f6bac21e7f99f73567815ed65e31
BlueberryWolf <blue@blueberry.coffee>
2025-05-15 14:04:22 -0600

    Revert "fixed more stuffs"
    This reverts commit 60ecb1d82608b5244c1ad2871ae8e6f3350314b0.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,7 +13,6 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
-        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -37,10 +36,6 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
-}
-
-dependencies {
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
 
 flutter {
