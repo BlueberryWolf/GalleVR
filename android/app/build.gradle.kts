f5f0c837e130917af21f15df6039348c364b4003
BlueberryWolf <blue@blueberry.coffee>
2026-04-24 09:04:26 -0600

    update android app id

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 android {
-    namespace = "com.gallevr.gallevr"
+    namespace = "com.gallevr.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -23,7 +23,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.gallevr.gallevr"
+        applicationId = "com.gallevr.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/gallevr/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/gallevr/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.gallevr.gallevr
+package com.gallevr.app
 
 import io.flutter.embedding.android.FlutterActivity
 
